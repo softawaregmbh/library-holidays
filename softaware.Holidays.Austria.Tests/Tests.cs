6100741c74ff2fd5a37e4966e2d604258378764e
Daniel Sklenitzka <Skleni@users.noreply.github.com>
2018-09-24 14:58:36 +0200

    Additional holidays (#2)
    * update version in holidays.austria
    
    * fathers day, mothers day, valentines day added
    
    * Refactoring, added Halloween

--- softaware.Holidays.Austria.Tests/Tests.cs
+++ softaware.Holidays.Austria.Tests/Tests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Xunit;
+using System.Linq;
 
 namespace softaware.Holidays.Austria.Tests
 {
@@ -14,20 +15,52 @@ namespace softaware.Holidays.Austria.Tests
             Assert.Equal(holidays, new List<Model.Holiday>() {
                 new Model.Holiday { Name = "Neujahr", Date = new DateTime(2018, 1, 1) },
                 new Model.Holiday { Name = "Heilige Drei Könige", Date = new DateTime(2018, 1, 6) },
+                new Model.Holiday { Name = "Valentinstag", Date = new DateTime(2018, 2, 14) },
                 new Model.Holiday { Name = "Karfreitag", Date = new DateTime(2018, 3, 30) },
                 new Model.Holiday { Name = "Ostersonntag", Date = new DateTime(2018, 4, 1) },
                 new Model.Holiday { Name = "Ostermontag", Date = new DateTime(2018, 4, 2) },
                 new Model.Holiday { Name = "Staatsfeiertag", Date = new DateTime(2018, 5, 1) },
+                new Model.Holiday { Name = "Muttertag", Date = new DateTime(2018, 5, 13) },
                 new Model.Holiday { Name = "Christi Himmelfahrt", Date = new DateTime(2018, 5, 10) },
                 new Model.Holiday { Name = "Pfingstmontag", Date = new DateTime(2018, 5, 21) },
                 new Model.Holiday { Name = "Fronleichnam", Date = new DateTime(2018, 5, 31) },
+                new Model.Holiday { Name = "Vatertag", Date = new DateTime(2018, 6, 10) },
                 new Model.Holiday { Name = "Mariä Himmelfahrt", Date = new DateTime(2018, 8, 15) },
                 new Model.Holiday { Name = "Nationalfeiertag", Date = new DateTime(2018, 10, 26) },
+                new Model.Holiday { Name = "Halloween", Date = new DateTime(2018, 10, 31) },
                 new Model.Holiday { Name = "Allerheiligen", Date = new DateTime(2018, 11, 1) },
                 new Model.Holiday { Name = "Mariä Empfängnis", Date = new DateTime(2018, 12, 8) },
                 new Model.Holiday { Name = "Christtag", Date = new DateTime(2018, 12, 25) },
                 new Model.Holiday { Name = "Stefanitag", Date = new DateTime(2018, 12, 26) },
             });
+        }
+
+        [Fact]
+        public void FathersDay()
+        {
+            var fathersday2018 = new Holidays.Generator().ForAustria(2018).Where(h => h.Name.Equals("Vatertag")).First().Date;
+            var fathersday2019 = new Holidays.Generator().ForAustria(2019).Where(h => h.Name.Equals("Vatertag")).First().Date;
+            var fathersday2020 = new Holidays.Generator().ForAustria(2020).Where(h => h.Name.Equals("Vatertag")).First().Date;
+            var fathersday2021 = new Holidays.Generator().ForAustria(2021).Where(h => h.Name.Equals("Vatertag")).First().Date;
+
+            Assert.Equal(fathersday2018, new DateTime(2018, 6, 10));
+            Assert.Equal(fathersday2019, new DateTime(2019, 6, 9));
+            Assert.Equal(fathersday2020, new DateTime(2020, 6, 14));
+            Assert.Equal(fathersday2021, new DateTime(2021, 6, 13));
+        }
+
+        [Fact]
+        public void MothersDay()
+        {
+            var mothersday2018 = new Holidays.Generator().ForAustria(2018).Where(h => h.Name.Equals("Muttertag")).First().Date;
+            var mothersday2019 = new Holidays.Generator().ForAustria(2019).Where(h => h.Name.Equals("Muttertag")).First().Date;
+            var mothersday2020 = new Holidays.Generator().ForAustria(2020).Where(h => h.Name.Equals("Muttertag")).First().Date;
+            var mothersday2021 = new Holidays.Generator().ForAustria(2021).Where(h => h.Name.Equals("Muttertag")).First().Date;
+
+            Assert.Equal(mothersday2018, new DateTime(2018, 5, 13));
+            Assert.Equal(mothersday2019, new DateTime(2019, 5, 12));
+            Assert.Equal(mothersday2020, new DateTime(2020, 5, 10));
+            Assert.Equal(mothersday2021, new DateTime(2021, 5, 9));
         }
     }
 }
--- softaware.Holidays.Austria/Generator.cs
+++ softaware.Holidays.Austria/Generator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using softaware.Holidays.Model;
 
 namespace softaware.Holidays.Austria
@@ -20,15 +21,19 @@ namespace softaware.Holidays.Austria
 
             yield return holiday.WithDate("Neujahr", month: 1, day: 1);
             yield return holiday.WithDate("Heilige Drei Könige", month: 1, day: 6);
+            yield return holiday.WithDate("Valentinstag", month: 2, day: 14, workingDay: true);
             yield return holiday.BeforeEaster("Karfreitag", days: 2);
             yield return holiday.AfterEaster("Ostersonntag", days: 0);
             yield return holiday.AfterEaster("Ostermontag", days: 1);
             yield return holiday.WithDate("Staatsfeiertag", month: 5, day: 1);
+            yield return holiday.NthDayInMonth("Muttertag", 2, DayOfWeek.Sunday, 5);
             yield return holiday.AfterEaster("Christi Himmelfahrt", days: 39);
             yield return holiday.AfterEaster("Pfingstmontag", days: 50);
             yield return holiday.AfterEaster("Fronleichnam", days: 60);
+            yield return holiday.NthDayInMonth("Vatertag", 2, DayOfWeek.Sunday, 6);
             yield return holiday.WithDate("Mariä Himmelfahrt", month: 8, day: 15);
             yield return holiday.WithDate("Nationalfeiertag", month: 10, day: 26);
+            yield return holiday.WithDate("Halloween", month: 10, day: 31, workingDay: true);
             yield return holiday.WithDate("Allerheiligen", month: 11, day: 1);
             yield return holiday.WithDate("Mariä Empfängnis", month: 12, day: 8);
             yield return holiday.WithDate("Christtag", month: 12, day: 25);
